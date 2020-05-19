343904a6135e631c8653e4d14cee7c34f69d75c4
PS-07 <singh.puneet1997@gmail.com>
2020-05-19 17:27:20 +0530

    version test

---
+++ ticketing/tickets/src/models/__test__/ticket.test.ts
@@ -0,0 +1,32 @@
+import { Ticket } from '../ticket';
+
+it('implements optimistic concurrency control', async (done) => {
+    // create an instance of a ticket
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 30,
+        userId: '104'
+    });
+
+    // save the ticket to database
+    await ticket.save()
+
+    // fetch the ticket twice
+    const firstInstance = await Ticket.findById(ticket.id);
+    const secondInstance = await Ticket.findById(ticket.id);
+
+    // make two separate changes to the ticket we fetched
+    firstInstance!.set({ price: 40 });
+    secondInstance!.set({ price: 50 });
+
+    // save the first fetched ticket
+    await firstInstance!.save();
+
+    // save the second fetched ticket and expect an error
+    try {
+        await secondInstance!.save();
+    } catch (err) {
+        return done();
+    }
+    throw new Error('should not reach this point');
+});
